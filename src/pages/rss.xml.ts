136feb13af45a4aefc970c4fbacf28816df4c9ae
alfredoh <alfredoh.ucv@gmail.com>
2026-01-10 19:28:33 -0400

    mejorando el contenido a mostrar por el rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,11 @@
 import type { APIRoute } from "astro"
-import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
+import rss from "@astrojs/rss"
+
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from "markdown-it"
+
+const parser = new MarkdownIt()
 
 export const GET: APIRoute = async ({ params, request, site }) => {
     const blogPosts = await getCollection("blog")
@@ -9,6 +14,11 @@ export const GET: APIRoute = async ({ params, request, site }) => {
         //stylesheet: '/styles/rss.xsl',
         // `<title>` field in output xml
         title: 'Tenexthop’s Blog',
+
+        xmlns: {
+            media: 'http://search.yahoo.com/mrss/',
+        },
+
         // `<description>` field in output xml
         description: 'Un blog simple de ejemplo para aprender a trabajar con el framework Astro',
         // Pull in your project "site" from the endpoint context
@@ -17,11 +27,23 @@ export const GET: APIRoute = async ({ params, request, site }) => {
 
         // Array of `<item>`s in output xml
         // See "Generating items" section for examples using content collections and glob imports
-        items: blogPosts.map(({ slug, data }) => ({
+        items: blogPosts.map(({ slug, data, body }) => ({
             title: data.title,
             description: data.description,
             pubDate: data.date,
             link: `/posts/${slug}`,
+
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+            }),
+
+            customData: `<media:content
+                type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                width="${data.image.width}"
+                height="${data.image.height}"
+                medium="image"
+                url="${site + data.image.src}" />
+            `,
 
         })),
         // (optional) inject custom xml
