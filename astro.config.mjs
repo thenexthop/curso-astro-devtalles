21b4c0a2d28f953de7a5a842bfd688d1fd2e1c10
alfredoh <alfredoh.ucv@gmail.com>
2026-01-11 14:31:15 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
