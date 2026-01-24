48d6e9a625f69b8e393d5da32329e56f20e3c2cd
thenexthop <alfredoh.ucv@gmail.com>
2026-01-24 09:06:39 -0400

    Delete .vercel/output directory

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,5 +0,0 @@
-import { c, a } from "./chunks/_@astrojs-ssr-adapter_Q5tXXscf.mjs";
-export {
-  c as createExports,
-  a as start
-};
--- .vercel/output/static/_astro/astro_scripts/before-hydration.js.BJcD1lMm.js
+++
@@ -1,1 +0,0 @@
-import{r}from"../index.DyMxUdkx.js";import"../chunk-MZTESQVU.DFZtgiu1.js";await r({sdkMetadata:{version:"2.16.13",name:"@clerk/astro",environment:"production"}});
